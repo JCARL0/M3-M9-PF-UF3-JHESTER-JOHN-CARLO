b0c6a9cb553295fb417d09ed0f1a22a165fa6c87
JCARL0 <john.magno@estudiant.fjaverianas.com>
2025-06-09 14:53:00 +0200

    PokeTCG

--- obj/Release/net8.0/PokeTCG.AssemblyInfo.cs
+++ obj/Release/net8.0/PokeTCG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeTCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc0177bc88c111ecdbde13f69c2c218a67c5ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa648967a4b42e156cac853604243ff1cde36357")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeTCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeTCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
